1a874b8b1a29141839d87b0241ef2dd5cfccb12b
vilho4 <ville.j.holopainen@student.jyu.fi>
2025-02-02 17:47:53 +0200

    3.18
    Fix url

--- Phonebook-back/models/person.js
+++ Phonebook-back/models/person.js
@@ -7,8 +7,6 @@ mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
-console.log(url,'mitävittua')
-
 console.log('connecting to', url)
 mongoose.connect(url)
   .then(result => {
